53b32acd4a922f8a27239e0a156096fde21416d2
Skip Townsend <skiptownsend@outlook.com>
2022-12-30 12:50:54 -0700

    Added automapping functionality to ReviewSite_Web project

---
+++ ReviewSite_Web/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ReviewSite_WebAPI.Models;
+using ReviewSite_WebAPI.Models.DTO;
+
+namespace ReviewSite_WebAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Product, ProductCreateDTO>().ReverseMap();
+            CreateMap<Product, ProductUpdateDTO>().ReverseMap();
+
+            CreateMap<ProductReview, ProductReviewDTO>().ReverseMap();
+            CreateMap<ProductReview, ProductReviewCreateDTO>().ReverseMap();
+            CreateMap<ProductReview, ProductReviewUpdateDTO>().ReverseMap();
+        }
+    }
+}
