11ccad331475300394643ebd1339fcf620b9273a
Skip Townsend <skiptownsend@outlook.com>
2022-12-30 12:26:46 -0700

    Added ReviewSite_Utility library to solution and APIRequest model to ReviewSite_Web project

---
+++ ReviewSite_Utility/SD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewSite_Utility
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE        }
+    }
+}
---
+++ ReviewSite_Web/Models/APIRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static ReviewSite_Utility.SD;
+
+namespace ReviewSite_Web.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
