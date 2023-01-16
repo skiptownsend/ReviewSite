927ef1db3bb9118ebdd3f9432f5e627a0fcafd60
Skip Townsend <skiptownsend@outlook.com>
2023-01-16 15:39:29 -0700

    Add images to home index page

--- ReviewSite_Web/Controllers/HomeController.cs
+++ ReviewSite_Web/Controllers/HomeController.cs
@@ -1,32 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using ReviewSite_Web.Models;
+using ReviewSite_Web.Models.DTO;
+using ReviewSite_Web.Services.IServices;
 using System.Diagnostics;
 
 namespace ReviewSite_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+        public HomeController(IProductService productService, IMapper mapper)
         {
-            _logger = logger;
+            _productService = productService;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            List<ProductDTO> list = new();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var response = await _productService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
         }
     }
 }
